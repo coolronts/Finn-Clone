ab00762093a8595f33ef60678e6f5bc9c384ff0e
Zahid Ronty <58170502+coolronts@users.noreply.github.com>
2021-07-21 23:40:44 +0200

    @Version 4.0.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,11 @@
-const colors = require("tailwindcss/colors");
-
 module.exports = {
-    purge: [],
+    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        backdropFilter: {
-            none: "none",
-            blur: "blur(20px)",
-        },
-
         extend: {},
     },
     variants: {
         extend: {},
     },
-    plugins: [require("tailwindcss-filters")],
+    plugins: [],
 };
